16bfbd9f2a41f80b7bd03929f33c15f73fa7fdb5
Danusha <danusha70754@gmail.com>
2022-12-02 00:15:17 +0530

    Update loadLinuxScript.groovy

--- vars/loadLinuxScript.groovy
+++ vars/loadLinuxScript.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) { 
   def scriptcontents = libraryResource "com/planetpope/scripts/windows/${config.name}"
   writeFile file: "${config.name}", text: scriptcontents
-  bat "chmod a+x ./${config.name}"
+  bat "./${config.name}"
 }
